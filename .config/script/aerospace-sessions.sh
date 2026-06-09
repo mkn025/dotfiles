6cc949b463c54521f18152eb463433bdb9fbed9e
Martin Elde Knutsen <maknu0536@uib.no>
2026-06-09 09:57:59 +0200

    add aerospace script

--- .config/script/aerospace-sessions.sh
+++ .config/script/aerospace-sessions.sh
@@ -1,1 +1,1 @@
-aerospace list-windows --all | fzf --tmux 80% --bind 'enter:execute(bash -c "aerospace focus --window-id {1}")+abort'
+aerospace list-windows --all | fzf  --bind 'enter:execute(bash -c "aerospace focus --window-id {1}")+abort'
